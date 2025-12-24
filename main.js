37b943e59aeaaa93119cf888f0a52946ba0ba8d3
AmirtterK <strikereureka7661@gmail.com>
2025-12-24 21:10:32 +0100

    text fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
-const DISPLAY_TEXT = "BOUCHRA";
+const DISPLAY_TEXT = "lil Nigga";
 
 const isMobileDevice = window.innerWidth < 768;
 
